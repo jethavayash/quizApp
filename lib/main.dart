1d49b53695cc90c218fa33a11b8fcc34dbed7129
yashJethava <jethavayash7@gmail.com>
2024-06-21 17:27:58 +0530

    first commit...

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+      ),
+      //  home: const MyHomePage(),
+    );
+  }
+}
+
+
---
+++ lib/splashScreen.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  bool visible = true;
+  AnimationController? animationController;
+  Animation<double>? animation;
+
+  startTime() async {
+    try {
+      var _duration = const Duration(seconds: 3);
+      return Timer(_duration, navigationPage);
+    } catch(error){
+      print('startTime Exception :- $error');
+    }
+  }
+
+  Future<void> navigationPage() async {
+    try {
+      final SharedPreferences preferences = await SharedPreferences.getInstance();
+      var name = preferences.getString('DisplayName');
+      var userId = preferences.getString('userId');
+      var userEmail = preferences.getString('email');
+
+      print('USERID=== ${userId}');
+      print('USER EMAIL=== ${userEmail}');
+      print('USER NAME=== ${name}');
+
+      if (userId == null) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => SignUpPage()),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => TaskListScreen(
+              user: Users(
+                id: int.tryParse(userId)!,
+                name: name ?? '',
+                email: userEmail ?? '',
+                isAdmin: false, // You can set admin status based on your logic
+              ),
+            ),
+          ),
+        );
+      }
+    } catch (error) {
+      print('navigationPage Exception :- $error');
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation =
+        CurvedAnimation(parent: animationController!, curve: Curves.easeInOut);
+    animation!.addListener(() => setState(() {
+      visible = !visible;
+    }));
+    animationController!.forward();
+    setState(() {
+      visible = !visible;
+    });
+    startTime();
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Center(
+          child: Text(
+            'Ellena ToDo App',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                color: Color(0xFFFFFFFF),
+                letterSpacing: 10.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
